7d5c6351031350e6fd2217e3c9adc3a72209f3f5
sanzzyproject <stokakunsanzz250@gmail.com>
2026-01-19 10:48:29 +0700

    Create models.py

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+class MenuItem(BaseModel):
+    name: str
+    price: int
+    qty: int = 1
+
+class TransactionRequest(BaseModel):
+    items: List[MenuItem]
+    paid: int
+
+class TransactionResponse(BaseModel):
+    items: List[MenuItem]
+    total: int
+    paid: int
+    change: int
+    timestamp: str
+    status: str
